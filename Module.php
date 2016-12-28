024b7e1d80df3fb17d296287a908004fc4069439
Jaimie Garner <pacificnm@gmail.com>
2016-12-28 14:18:27 -0800

    Updates to 1.0.3

--- Module.php
+++ Module.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Pacific NM (https://www.pacificnm.com)
+ *
+ * @link      https://github.com/pacificnm/pacificnm-acl-resource for the canonical source repository
+ * @copyright Copyright (c) 20011-2016 Pacific NM USA Inc. (https://www.pacificnm.com)
+ * @license BSD-3-Clause
+ */
 namespace AclResource;
 
 class Module
--- sql/acl_resource.sql
+++ sql/acl_resource.sql
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `acl_resource` (
 `acl_resource_id` int(20) unsigned NOT NULL,
   `acl_resource_name` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB;
 
 --
 -- Indexes for dumped tables
