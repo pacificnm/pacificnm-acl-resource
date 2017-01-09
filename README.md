8def4bc0fbe332688df0aaf6efab400b68b34778
Jaimie Garner <pacificnm@gmail.com>
2017-01-09 10:05:51 -0800

    Adds menu items

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ Requires pacificnm-acl
 File issues at https://github.com/pacificnm/pacificnm-acl/issues
 
 Documentation is at https://github.com/pacificnm/pacificnm-acl/
+
+
+# 1.0.6
+Updates form views adds default display partial.
--- config/pacificnm.acl-resource.global.php
+++ config/pacificnm.acl-resource.global.php
@@ -10,7 +10,7 @@ return array(
     'module' => array(
         'AclResource' => array(
             'name' => 'AclResource',
-            'version' => '1.0.5',
+            'version' => '1.0.6',
             'install' => array(
                 'require' => array(),
                 'sql' => 'sql/acl_resource.sql'
